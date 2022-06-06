3d5da96ac240a82aee94399c0fa2f8acecd4c0a6
tzrlk <peterc@aetheric.co.nz>
2022-06-06 21:47:30 +1200

    Updating cdktf config

--- src/cdktf/secret.ts
+++ src/cdktf/secret.ts
@@ -1,14 +1,24 @@
-import { TerraformStack, TerraformVariable  } from "cdktf";
+import { TerraformVariable } from "cdktf";
+import { Construct } from 'constructs';
+import { SecretsmanagerSecret, SecretsmanagerSecretVersion } from '../../.gen/providers/aws/secretsmanager';
 
+export function aws_secret(stack: Construct, id: string, name: string, desc: string) {
 
-export function aws_secret(stack: TerraformStack, id: string, name: string, desc: string) {
-
-	new TerraformVariable(stack, id, {
+	const input = new TerraformVariable(stack, id, {
 		description: desc,
 		type: 'string',
 		nullable: false,
+		sensitive: true,
 	})
-	
-	new AwsSecretsmanagerSecret()
+
+	const secret = new SecretsmanagerSecret(stack, id, {
+		description: desc,
+		namePrefix: `${name}-`,
+	})
+
+	new SecretsmanagerSecretVersion(stack, id, {
+		secretId: secret.id,
+		secretString: input.stringValue,
+	})
 
 }
--- src/cdktf/valheim-ecs.ts
+++ src/cdktf/valheim-ecs.ts
@@ -1,18 +1,34 @@
-import { App, TerraformStack } from 'cdktf';
-import {
-	DataAwsRegion,
-	AwsProvider,
-} from '@cdktf/provider-aws';
+import { App, TerraformStack, TerraformVariable } from 'cdktf';
+import { IacValheimTf } from '../../.gen/modules/iac-valheim-tf';
+import { AwsProvider } from '../../.gen/providers/aws';
+import { DockerProvider } from '../../.gen/providers/docker';
 
 export class ValheimEcs extends TerraformStack {
 	constructor(scope: App) {
 		super(scope, 'valheim-ecs');
 
 		new AwsProvider(this, 'aws', {
-			region: 'ap-southeast-2',
-		});
+			region: 'ap-southeasst-2',
+			defaultTags: {
+				tags: {
+					App: 'Valheim',
+				},
+			}
+		})
 
-		new DataAwsRegion(this, 'region');
+		new DockerProvider(this, 'docker', {})
+
+		const adminList = new TerraformVariable(this, 'AdminList', {
+			description: 'The list of Steam Ids to nominate as admins.',
+			type: 'string[]',
+		})
+
+		new IacValheimTf(this, 'vhtf', {
+			adminList: adminList.listValue,
+			runServer: false,
+			serverPass: '',
+			sketchy: true,
+		})
 
 	}
 }
